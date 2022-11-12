3127c69e5806d78ab942766813411a8b5d2f4ea7
dragos0804 <43861207+dragos0804@users.noreply.github.com>
2022-11-12 22:42:51 +0200

    Implemented functions that serve as a base for future methods.

--- ProjectModern/ProjectModern/Login.cpp
+++ ProjectModern/ProjectModern/Login.cpp
@@ -1,5 +1,31 @@
 #include "Login.h"
 
+Login::Login(std::string usernameInput, std::string passwordInput)
+	: m_usernameInput(usernameInput)
+	, m_passwordInput(passwordInput)
+{
+}
+
+std::string Login::GetUsernameInput() const
+{
+	return m_usernameInput;
+}
+
+std::string Login::GetPasswordInput() const
+{
+	return m_passwordInput;
+}
+
+void Login::SetUsernameInput(const std::string& username)
+{
+	m_usernameInput = username;
+}
+
+void Login::SetPasswordInput(const std::string& password)
+{
+	m_passwordInput = password;
+}
+
 bool Login::VerifyMatchUserToPassword(const User& user)
 {
 	return user.GetUsername() == m_usernameInput && user.GetPassword() == m_passwordInput;
--- ProjectModern/ProjectModern/Login.h
+++ ProjectModern/ProjectModern/Login.h
@@ -3,10 +3,17 @@
 #include "User.h"
 class Login
 {
+public:
+	Login(std::string usernameInput, std::string passwordInput);
+
+	std::string GetUsernameInput() const;
+	std::string GetPasswordInput() const;
+	void SetUsernameInput(const std::string& username);
+	void SetPasswordInput(const std::string& password);
+
+	bool VerifyMatchUserToPassword(const User& user);
 private:
 	std::string m_usernameInput;
 	std::string m_passwordInput;
-public:
-	bool VerifyMatchUserToPassword(const User& user);
 };
 
