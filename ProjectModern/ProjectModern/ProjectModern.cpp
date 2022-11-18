f36bcd2b44ec44f36d5f21f3ad43b14f32415cbf
denisachete <denisachete30@gmail.com>
2022-11-18 16:16:58 +0200

    Tested email validation method.

--- ProjectModern/ProjectModern/ProjectModern.cpp
+++ ProjectModern/ProjectModern/ProjectModern.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "User.h"
+#include "Register.h"
+#include "IRegister.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    std::string email = "denisachete1223yahoo.com";
+    Register r(1, "Denisa Chete", "denic30", "12-12-2022", email, "parola", "Romania");
+
+
+    if (r.EmailValidation(email))
+    {
+        std::cout << "Email valid";
+    }
+    else
+    {
+        std::cout << "Email invalid";
+    }
 }
