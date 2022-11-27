e40c6041fe492a719a105747ee636ad459b44b0c
Dvrkness <radubala45@yahoo.com>
2022-11-27 23:54:58 +0200

    Added implementation for GetSimilarBooks.

--- ProjectModern/ProjectModern/KMeans.cpp
+++ ProjectModern/ProjectModern/KMeans.cpp
@@ -1,1 +1,7 @@
 #include "KMeans.h"
+
+std::vector<Film> KMeans::GetSimilarFilms(const Film& film)
+{
+	uint8_t closetClusterIndex = ComputeClosetClusterIndex(film);
+	return m_clusteredFilms[closetClusterIndex];
+}
