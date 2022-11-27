fd5ad3f6409746e6ab74a30b5356c78c93826bc8
Dvrkness <radubala45@yahoo.com>
2022-11-27 23:33:40 +0200

    Added functions and definitions in KMeans.h.

--- ProjectModern/ProjectModern/KMeans.h
+++ ProjectModern/ProjectModern/KMeans.h
@@ -4,8 +4,16 @@
 
 class KMeans
 {
+public:
+	std::vector<Film> GetSimilarFilms(const Film& film);
 
 private:
+	float ComputeEuclidianDistance(Position firstPoint, Position secondPoint);
+	Position GetNormFilm(const Film& film);
+	uint8_t ComputeClosetClusterIndex(const Film& film);
+
+private:
+	
 	std::vector<Cluster> m_clusters;
 	std::vector<std::vector<Film>> m_clusteredFilms;
 };
