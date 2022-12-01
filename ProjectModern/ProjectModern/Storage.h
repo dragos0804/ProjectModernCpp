7457a343297cdc219c8fdde5d6585d91ea6be32e
denisachete <denisachete30@gmail.com>
2022-12-01 14:07:52 +0200

    Remade the table for films so that it suits the new enhancements of the class Film.

--- ProjectModern/ProjectModern/Storage.h
+++ ProjectModern/ProjectModern/Storage.h
@@ -22,19 +22,25 @@ inline auto createStorage(const std::string& filename)
 			make_column("email", &User::SetEmail, &User::GetEmail),
 			make_column("password", &User::SetPassword, &User::GetPassword),
 			make_column("country", &User::SetCountry, &User::GetCountry)
-		)/*,
+		),
+		 //show_id	type	title	director	cast	country	-date_added-	release_year	rating	duration	listed_in	description
+
 		make_table(
 			"Films",
-			make_column("id", &Film::SetId, &Film::GetId, autoincrement(), primary_key()),
-			make_column("name", &Film::SetName, &Film::GetName),
-			make_column("producers", &Film::SetProducers, &Film::GetProducers),
-			make_column("actors", &Film::SetActors, &Film::GetActors),
-			make_column("genre", &Film::SetGenres, &Film::GetGenres),
-			make_column("release_date", &Film::SetReleaseDate, &Film::GetReleaseDate),
+			make_column("id", &Film::SetId, &Film::GetId, autoincrement(), primary_key()), 
+			make_column("type", &Film::SetType, &Film::GetType), 
+			make_column("title", &Film::SetTitle, &Film::GetTitle), 
+			make_column("director", &Film::SetDirector, &Film::GetDirector), 
+			make_column("cast", &Film::SetCast, &Film::GetCast),
+			make_column("country", &Film::SetCountry, &Film::GetCountry),
+			make_column("release_year", &Film::SetReleaseYear, &Film::GetReleaseYear), 
+			make_column("age_range", &Film::SetAgeRange, &Film::GetAgeRange),
 			make_column("duration", &Film::SetDuration, &Film::GetDuration),
+			make_column("genre", &Film::SetGenres, &Film::GetGenres),
+			make_column("description", &Film::SetDescription, &Film::GetDescription),
 			make_column("rating", &Film::SetRating, &Film::GetRating),
 			make_column("number_of_reviews", &Film::SetNumberOfReviews, &Film::GetNumberOfReviews)
-		),
+		)/*,
 		make_table(
 			"Series",
 			make_column("id", &Film::SetId, &Film::GetId, autoincrement(), primary_key()),
@@ -48,8 +54,6 @@ inline auto createStorage(const std::string& filename)
 			make_column("number_of_reviews", &Film::SetNumberOfReviews, &Film::GetNumberOfReviews)
 		)*/
 	);
-
-	//TODO: type, country,date_added, listed_in,description
 }
 
 using Storage = decltype(createStorage(""));
