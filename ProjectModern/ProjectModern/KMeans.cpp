15431e04f34de5ee4914f293114ef84d17c9a0a8
Dvrkness <radubala45@yahoo.com>
2022-12-09 10:19:37 +0200

    Created template function to iterate through the points of the cluster. Fixed the logic behind it by using the power2 macro.

--- ProjectModern/ProjectModern/KMeans.cpp
+++ ProjectModern/ProjectModern/KMeans.cpp
@@ -1,25 +1,9 @@
 #include "KMeans.h"
-#define power2(a) ((a) * (a))
 
 std::vector<Film> KMeans::GetSimilarFilms(const Film& film)
 {
 	uint8_t closetClusterIndex = ComputeClosetClusterIndex(film);
 	return m_clusteredFilms[closetClusterIndex];
-}
-
-float KMeans::ComputeEuclidianDistance(Position firstPoint, Position secondPoint)
-{
-	float total = 0;
-	uint8_t numberOfCoordinates = std::tuple_size<decltype(firstPoint)>::value;
-	
-	total += (float)(std::get<0>(secondPoint) - std::get<0>(firstPoint));
-	total += (float)(std::get<1>(secondPoint) - std::get<1>(firstPoint));
-	total += (float)(std::get<2>(secondPoint) - std::get<2>(firstPoint));
-	total += (float)(std::get<3>(secondPoint) - std::get<3>(firstPoint));
-	total += (float)(std::get<4>(secondPoint) - std::get<4>(firstPoint));
-
-	return sqrt(total);
-
 }
 
 Position KMeans::GetNormFilm(const Film& film)
--- ProjectModern/ProjectModern/KMeans.h
+++ ProjectModern/ProjectModern/KMeans.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Cluster.h"
 #include "Film.h"
+#define power2(a) ((a) * (a))
 
 class KMeans
 {
@@ -8,7 +9,16 @@ public:
 	std::vector<Film> GetSimilarFilms(const Film& film);
 
 private:
-	float ComputeEuclidianDistance(Position firstPoint, Position secondPoint);
+	template <size_t I = 0, typename... Ts>
+	float ComputeEuclidianDistance(std::tuple<Ts...> firstPoint, std::tuple<Ts...> secondPoint)
+	{
+		float total = 0;
+		for (; I != sizeof..(Ts); I++)
+		{
+			total += power2(std::get<I>(secondPoint) - std::get<I>(firstPoint));
+		}
+		return sqrt(total);
+	}
 	Position GetNormFilm(const Film& film);
 	uint8_t ComputeClosetClusterIndex(const Film& film);
 
@@ -17,4 +27,5 @@ private:
 	std::vector<Cluster> m_clusters;
 	std::vector<std::vector<Film>> m_clusteredFilms;
 };
+
 
