8713d62c48e571a99c6de53dd1cc6b001ab9f12a
Dvrkness <radubala45@yahoo.com>
2022-12-11 13:34:43 +0200

    Implemented possibility of exiting the application through pressing ESC key and going back to the login menu in case of a failed login attempt.

--- ProjectModern/ProjectModern/ProjectModern.cpp
+++ ProjectModern/ProjectModern/ProjectModern.cpp
@@ -16,103 +16,103 @@ int main()
     std::string username = "_denisa30";
     std::string dateOfBirth = "2002-01-30";
 
-   /* std::vector<User> users;
-    
-    if (r.CheckUniqueUsername(users))
-        std::cout << "registered" << std::endl;
-    User user1(1, "Denisa Chete", username, "12-12-2022", email, "parola", "Romania");
-    users.push_back(user1);
-    if (r.CheckUniqueUsername(users))
-        std::cout << "registration failed" << std::endl;
+    /* std::vector<User> users;
 
-    if (r.EmailValidation(email))
-    {
-        std::cout << "Email valid" << std::endl;
-    }
-    else
-    {
-        std::cout << "Email invalid" << std::endl;
-    }
+     if (r.CheckUniqueUsername(users))
+         std::cout << "registered" << std::endl;
+     User user1(1, "Denisa Chete", username, "12-12-2022", email, "parola", "Romania");
+     users.push_back(user1);
+     if (r.CheckUniqueUsername(users))
+         std::cout << "registration failed" << std::endl;
 
-    if (r.UsernameValidation(username, users))
-    {
-        std::cout << "Username valid" << std::endl;
-    }
-    else
-    {
-        std::cout << "Username invalid" << std::endl;
-    }*/
+     if (r.EmailValidation(email))
+     {
+         std::cout << "Email valid" << std::endl;
+     }
+     else
+     {
+         std::cout << "Email invalid" << std::endl;
+     }
 
-    /*if (r.AgeValidation(dateOfBirth))
-    {
-        std::cout << "Valid age";
-    }
-    else
-    {
-        std::cout << "Invalid age";
-    }*/
+     if (r.UsernameValidation(username, users))
+     {
+         std::cout << "Username valid" << std::endl;
+     }
+     else
+     {
+         std::cout << "Username invalid" << std::endl;
+     }*/
 
-  //  User u;
-  //  u.SetUsername(username);
-  //  u.SetPassword("parola");
+     /*if (r.AgeValidation(dateOfBirth))
+     {
+         std::cout << "Valid age";
+     }
+     else
+     {
+         std::cout << "Invalid age";
+     }*/
 
-  //  Login l(username, "parola");
-  //  if (l.VerifyMatchUserToPassword(u) == true)
-  //      std::cout << "Login successful" << std::endl;
+     //  User u;
+     //  u.SetUsername(username);
+     //  u.SetPassword("parola");
 
-  //  Login l1(username, "parolaP");
-  //  if (l1.VerifyMatchUserToPassword(u) == false)
-  //      std::cout << "Login failed" << std::endl;
+     //  Login l(username, "parola");
+     //  if (l.VerifyMatchUserToPassword(u) == true)
+     //      std::cout << "Login successful" << std::endl;
 
-  //  Film f1(0, 7, 200, 5, "film1", "20-12-2020", { Film::Genre::action, Film::Genre::adventure }, { Film::Actor::A1 }, { Film::Producer::P1 });
-  //  Film f2(1, 3, 100, 4, "film2", "21-12-2020", { Film::Genre::action, Film::Genre::adventure }, { Film::Actor::A2 }, { Film::Producer::P2 });
+     //  Login l1(username, "parolaP");
+     //  if (l1.VerifyMatchUserToPassword(u) == false)
+     //      std::cout << "Login failed" << std::endl;
 
-  //  std::vector<Film> movies;
-  //  movies.push_back(f1);
-  //  movies.push_back(f2);
+     //  Film f1(0, 7, 200, 5, "film1", "20-12-2020", { Film::Genre::action, Film::Genre::adventure }, { Film::Actor::A1 }, { Film::Producer::P1 });
+     //  Film f2(1, 3, 100, 4, "film2", "21-12-2020", { Film::Genre::action, Film::Genre::adventure }, { Film::Actor::A2 }, { Film::Producer::P2 });
 
-  //  MovieFiltering mv;
-  //  mv.SortBy(0);
-  //  for (auto& movie : movies)
-  //      std::cout << movie.GetName() << " ";
-  //  std::cout << std::endl;
+     //  std::vector<Film> movies;
+     //  movies.push_back(f1);
+     //  movies.push_back(f2);
 
-  //  mv.SortBy(1);
-  //  for (auto& movie : movies)
-  //      std::cout << movie.GetName() << " ";
-  //  std::cout << std::endl;
+     //  MovieFiltering mv;
+     //  mv.SortBy(0);
+     //  for (auto& movie : movies)
+     //      std::cout << movie.GetName() << " ";
+     //  std::cout << std::endl;
 
-  //  mv.SortBy(2);
-  //  for (auto& movie : movies)
-  //      std::cout << movie.GetName() << " ";
-  //  std::cout << std::endl;
+     //  mv.SortBy(1);
+     //  for (auto& movie : movies)
+     //      std::cout << movie.GetName() << " ";
+     //  std::cout << std::endl;
 
-  //  mv.SortBy(3);
-  //  for (auto& movie : movies)
-  //      std::cout << movie.GetName() << " ";
-  //  std::cout << std::endl;*/
+     //  mv.SortBy(2);
+     //  for (auto& movie : movies)
+     //      std::cout << movie.GetName() << " ";
+     //  std::cout << std::endl;
 
-  //  r.SetPassword("ParolaNoua1");
-  //  r.PasswordValidation(r.GetPassword());
-  //  r.SetPassword("ParolaPuternica123!");
-  //  r.PasswordValidation(r.GetPassword());
+     //  mv.SortBy(3);
+     //  for (auto& movie : movies)
+     //      std::cout << movie.GetName() << " ";
+     //  std::cout << std::endl;*/
 
-   //ParseMethod();
+     //  r.SetPassword("ParolaNoua1");
+     //  r.PasswordValidation(r.GetPassword());
+     //  r.SetPassword("ParolaPuternica123!");
+     //  r.PasswordValidation(r.GetPassword());
 
-   // User user1(1, "Denisa Chete", username, "12-12-2022", email, "parola", "Romania");
-   
-   // storage.AddUser(user1);
+      //ParseMethod();
 
-   // Film film1(1, 5, 120, 3.5, "Film", "Dick Johnson Is Dead", "TV-MA", "After crossing paths at a party, a Cape Town teen sets out to prove whether a private-school swimming star is her sister who was abducted at birth.", 2016, "Action, Drama", "", "Kirsten Johnson", "United States, Romania");
-   // storage.AddFilm(film1);
+      // User user1(1, "Denisa Chete", username, "12-12-2022", email, "parola", "Romania");
+
+      // storage.AddUser(user1);
+
+      // Film film1(1, 5, 120, 3.5, "Film", "Dick Johnson Is Dead", "TV-MA", "After crossing paths at a party, a Cape Town teen sets out to prove whether a private-school swimming star is her sister who was abducted at birth.", 2016, "Action, Drama", "", "Kirsten Johnson", "United States, Romania");
+      // storage.AddFilm(film1);
 
 
-  AppStorage storage;
+    AppStorage storage;
 
     Register r;
     int option = 1;
     int keyCheck = -1;
-    
+
     while (1) {
         switch (option)
         {
@@ -135,7 +135,7 @@ int main()
 
             //std::cin >> option;
             //std::cin.get();
-            
+
             // WINDOWS Specific = reads character ( also special ones ) without echoing and returns ASCII value
             option = _getch();
             break;
@@ -155,6 +155,12 @@ int main()
             if (keyCheck == 8) {
                 option = 1;
                 system("CLS");
+                break;
+            }
+
+            // ESC key -> 27 in ASCII Table
+            else if (keyCheck == 27) {
+                option = -1;
                 break;
             }
 
@@ -182,6 +188,13 @@ int main()
                 break;
             }
 
+            // ESC key -> 27 in ASCII Table
+            else if (keyCheck == 27) {
+                option = -1;
+                break;
+            }
+
+
             std::cout << "\t\tUsername: ";
             std::getline(std::cin, username);
             std::cout << "\t\tPassword: ";
@@ -205,13 +218,18 @@ int main()
                 std::cout << "\t\t|                                                    |\n";
                 std::cout << "\t\t+----------------------------------------------------+\n\n";
                 std::cout << "\t\tPress ENTER to continue...\n";
-            }  
+            }
+            else {
+                option = 50;
+                Sleep(4000);
+                break;
+            }
         }
         default:
             return 0;
         }
     }
-    
+
 
     return 0;
 }
