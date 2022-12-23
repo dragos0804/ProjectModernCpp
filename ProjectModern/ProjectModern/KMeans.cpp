a813327bf80ad21fa7603cf6e5a5e67135544679
Alex Chelmus <chelmus.alexanther@gmail.com>
2022-12-23 21:30:13 +0200

    Prepared map for clustering.

--- ProjectModern/ProjectModern/KMeans.cpp
+++ ProjectModern/ProjectModern/KMeans.cpp
@@ -1,5 +1,51 @@
 #include "KMeans.h"
 
+const std::unordered_map<std::string, float> CategoriesMappingValues =
+{
+	{"TV Shows", 0.0f},
+	{"Teen TV Shows", 0.0f},
+	{"TV Sci-Fi & Fantasy", 0.0f},
+	{"Spanish-Language TV Shows", 0.0f},
+	{"TV Horror", 0.0f},
+	{"TV Comedies", 0.0f},
+	{"Docuseries", 0.0f},
+	{"TV Action & Adventure", 0.0f},
+	{"Crime TV Shows", 0.0f},
+	{"Dramas", 0.0f},
+	{"TV Mysteries", 0.0f},
+	{"Romantic TV Shows", 0.0f},
+	{"Movies", 0.0f},
+	{"Sports Movies", 0.0f},
+	{"Independent Movies", 0.0f},
+	{"Children & Family Movies", 0.0f},
+	{"Korean TV Shows", 0.0f},
+	{"Music & Musicals", 0.0f},
+	{"Sci-Fi & Fantasy", 0.0f},
+	{"British TV Shows", 0.0f},
+	{"Stand-Up Comedy", 0.0f},
+	{"Horror Movies", 0.0f},
+	{"Science & Nature TV", 0.0f},
+	{"Comedies", 0.0f},
+	{"Anime Series", 0.0f},
+	{"Kids' TV", 0.0f},
+	{"Faith & Spirituality", 0.0f},
+	{"Stand-Up Comedy & Talk Shows", 0.0f},
+	{"TV Thrillers", 0.0f},
+	{"International Movies", 0.0f},
+	{"Reality TV", 0.0f},
+	{"International TV Shows", 0.0f},
+	{"Anime Features", 0.0f},
+	{"Romantic Movies", 0.0f},
+	{"LGBTQ Movies", 0.0f},
+	{"Documentaries", 0.0f},
+	{"Cult Movies", 0.0f},
+	{"TV Dramas", 0.0f},
+	{"Classic Movies", 0.0f},
+	{"Thrillers", 0.0f},
+	{"Classic & Cult TV", 0.0f},
+	{"Action & Adventure", 0.0f}
+};
+
 std::vector<Film> KMeans::GetSimilarFilms(const Film& film)
 {
 	uint8_t closestClusterIndex = ComputeClosestClusterIndex(film);
@@ -27,7 +73,11 @@ void KMeans::Run(std::vector<Film> films)
 
 Position KMeans::GetNormFilm(const Film& film)
 {
-	return Position();
+	/*return { film.rating,
+			CategoriesMappingValues.at(film.genres),
+			film.releaseYear,
+			film.duration,
+			film.ageRange };*/
 }
 
 uint8_t KMeans::ComputeClosestClusterIndex(const Film& film)
